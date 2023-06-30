8e60a41a9ae8e8eb99eccf452883be7270a12ecb
Jurij <jurij@MacBook-Air-Jurij.local>
2023-06-30 18:43:29 +0300

    Add existing project to git

---
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import s from './Post.module.css';
+
+const Post = (props) => {
+    return (
+        <div className={s.item}>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSIsmpJQm0OTBcGyY-Y3ECq4UMpN2lAcagoQ&usqp=CAU" alt=""/>
+            {props.message}
+            <div>
+                <span className={s.likeCounter}>{props.likeCounter}</span>
+                <span>Like</span>
+            </div>
+        </div>
+    )
+}
+
+export default Post;
